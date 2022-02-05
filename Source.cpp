ab8537bd9a746943febc462cda0241ebb14069db
HerrBearlin <79842500+HerrBearlin@users.noreply.github.com>
2022-02-05 12:40:01 +0100

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+using namespace std; //used to avoid typing std out and using names (it says import all names std into my program)
+int main() {
+	cout << "Hello, World" << endl;
+
+}
